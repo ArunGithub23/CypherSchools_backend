e5189b1cc666712bb51b48b9e0632aeda498c7d7
iamarungupta <arungupta21-24@bhavans.ac.in>
2023-04-14 05:24:38 +0530

    created endpoints of update password and others

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 
 
-const express=require('express');
+const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('./model/usermodel');
-const cors=require('cors');
+const cors = require('cors');
 
-app=express();
+app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -18,158 +18,170 @@ app.use(express.json());
 // Use the login and register routes as middleware//
 //app.use('/register', router);
 app.post('/register', async (req, res) => {
-    try {
-      const { firstName, lastName, email, password } = req.body;
-      // Check if user with same email already exists
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'User with the same email already exists' });
-      }
-      // Create new user
-      console.log("above fname")
-  
-      console.log(req.body.firstName)
-  
-      const salt = await bcrypt.genSalt(10);
-      console.log("salt okk")
-  
-      const hashedPassword = await bcrypt.hash(password, salt);
-      console.log("hash okk")
-  
-      const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword
-      });
-      await newUser.save();
-      res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error registering user' });
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    // Check if user with same email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User with the same email already exists' });
     }
-  });
-  
-  // Login user
-  app.post('/login', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      // Check if user with email exists
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid email or password' });
-      }
-      // Check if password is correct
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid email or password' });
-      }
-      res.status(200).json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error logging in user' });
+    // Create new user
+    console.log("above fname")
+
+    console.log(req.body.firstName)
+
+    const salt = await bcrypt.genSalt(10);
+    console.log("salt okk")
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hash okk")
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error registering user' });
+  }
+});
+
+// Login user
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Check if user with email exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
     }
-  });
-  
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error logging in user' });
+  }
+});
+
 // get endpoint by using _id
 
 app.get('/profile/:id', async (req, res) => {
-  const _id=req.params.id;
+  const _id = req.params.id;
   try {
-    const user = await User.findOne({_id});
+    const user = await User.findOne({ _id });
     console.log(user)
     res.status(200).json(user);
   } catch (error) {
     console.log(error.message);
-    res.status(400).json({message:"Error !!!"});
+    res.status(400).json({ message: "Error !!!" });
   }
- 
+
 });
 
 // Endpoint for updating user's about
-app.put('/about/:id',async(req,res)=>{
-  const _id=req.params.id;
-  const about=req.body.about;
+app.put('/about/:id', async (req, res) => {
+  const _id = req.params.id;
+  const about = req.body.about;
   try {
-    const user=await User.findByIdAndUpdate({_id},{about},{new:true})
+    const user = await User.findByIdAndUpdate({ _id }, { about }, { new: true })
     console.log(user)
     res.status(200);
   } catch (error) {
     console.log(error.message)
-    res.status(400).json({message:"error"})
+    res.status(400).json({ message: "error" })
   }
 })
 
 
-  
-  // Endpoint for updating user profile details
-  app.put('/profile', async (req, res) => {
-      const { firstName, lastName, email, image, links } = req.body;
-    
-      try {
-        // Find the user by email and update the profile details
-        const user = await User.findOneAndUpdate({ email }, { firstName, lastName, image, links }, { new: true });
-        res.json({ message: 'User profile updated successfully', user });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error updating user profile' });
-      }
-    });
-    
-  
-    // Endpoint for updating user password
-  app.put('/password', async (req, res) => {
-    const { email, oldPassword, newPassword } = req.body;
-  
-    try {
-      // Find the user by email and update the password
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(400).json({ message: 'User with the given email does not exist' });
-      }
-      const isPasswordCorrect = await user.comparePassword(oldPassword);
-      if (!isPasswordCorrect) {
-        return res.status(400).json({ message: 'Invalid password' });
-      }
-      user.password = newPassword;
-      await user.save();
-      res.json({ message: 'User password updated successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error updating user password' });
+
+// Endpoint for updating user profile details
+app.put('/profile', async (req, res) => {
+  const { firstName, lastName, email, image, links } = req.body;
+
+  try {
+    // Find the user by email and update the profile details
+    const user = await User.findOneAndUpdate({ email }, { firstName, lastName, image, links }, { new: true });
+    res.json({ message: 'User profile updated successfully', user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating user profile' });
+  }
+});
+
+
+// Endpoint for updating user password
+app.put('/password', async (req, res) => {
+  const { currentpassword, newpassword } = req.body.password;
+  const email = req.body.email;
+
+
+  try {
+    // Find the user by email and update the password
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'User with the given email does not exist' });
     }
-  });
 
-  //Endpoint to updata the weblinks
 
-  app.put('/weblinks/:id',async(req,res)=>{
-    const _id=req.params.id;
-    const links=req.body.links;
-    try {
-      const user=await User.findByIdAndUpdate({_id},{links},{new:true})
-      console.log(user)
-      res.status(200).json(user.links);
-    } catch (error) {
-      console.log(error.message)
-      res.status(400).json({message:"error"})
+    const isMatch = await bcrypt.compare(newpassword, user.password);
+    console.log("salt okk",isMatch)
+
+    if (isMatch) {
+      return res.status(400).json({ message: 'Invalid  password' });
     }
-  })
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+    const salt = await bcrypt.genSalt(10);
+    console.log("salt okk")
+
+    const hashedPassword = await bcrypt.hash(newpassword, salt);
+    console.log("hash okk")
+    user.password = hashedPassword;
+    await user.save();
+    res.json({ message: 'User password updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating user password' });
+  }
+});
+
+//Endpoint to updata the weblinks
+
+app.put('/weblinks/:id', async (req, res) => {
+  const _id = req.params.id;
+  const links = req.body.links;
+  try {
+    const user = await User.findByIdAndUpdate({ _id }, { links }, { new: true })
+    console.log(user)
+    res.status(200).json(user.links);
+  } catch (error) {
+    console.log(error.message)
+    res.status(400).json({ message: "error" })
+  }
+})
 
 
-app.listen(4000,  function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", 4000);
-   
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, function (err) {
+  if (err) console.log("Error in server setup")
+  console.log("Server listening on Port", 4000);
+
 
 
 });
